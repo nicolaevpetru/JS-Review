ac8e42264c1a55fc99f0a84c797dfb12711a73d5
Petru <nicolaevpetru@outlook.com>
2021-05-30 11:23:54 -0400

    Updated 1-variable.js

--- 1-variable.js
+++ 1-variable.js
@@ -1,1 +1,40 @@
 console.log('Hello world!');
+
+// when you declare a variable without spec. data type is called dinamic declaration in JS
+var count;
+count = 5;
+console.log(count);
+
+var i = 100;
+i = 10;
+console.log(i);
+
+
+// ES15(ecmascript2015), ES16(ecmascript2016) -> this is how the versions in JS are named
+
+
+var x = 5, y = 'String';
+console.log(x+y);
+console.log(x*y);
+
+
+// variable naming rules are the same as Java
+
+// Primitive Data Types:
+// Numbers(including double and integers example from Java) : 12345 or 394.99
+// String : 'String data type'
+// Boolean : true, false
+// Undifined : variable does not have a value yet
+// Null
+
+
+var a;
+console.log(a);
+
+console.log(typeof y);
+
+
+
+// prompt(scanner for Java) operator
+var name = prompt('Enter your name: ');
+console.log(name);
